5d356d8bef81a09bc22ae39bfe08efdafe4b19b2
Sithum-Ravishka <79629419+Sithum-Ravishka@users.noreply.github.com>
2022-01-31 22:43:13 +0530

    Route Update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Container } from '@material-ui/core';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 import Navbar from './components/Navbar/Navbar';
@@ -10,15 +9,13 @@ const App = () => {
 
   return (
     <BrowserRouter>
-      <Container maxWidth="xl">
         <Navbar />
         <Switch>
           <Route path="/" exact component={() => <Redirect to="/movies" />} />
           <Route path="/movies" exact component={Home} />
           <Route path="/movies/search" exact component={Home} />
-          <Route path={['/creators/:name', '/tags/:name']} component={CreatorOrTag} />
+          <Route path={'/tags/:name'} component={CreatorOrTag} />
         </Switch>
-      </Container>
     </BrowserRouter>
   );
 };
